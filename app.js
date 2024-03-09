d566f64667e1fb80f6a6c54bd971b0727039ab2f
jarvis09 <contact-jarvis09@laposte.net>
2024-03-09 20:28:54 +0100

    added console.log() for image credits

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ function getRandomBackgroundImage() {
 
 function changeBackgroundImage(img) {
   document.body.style.backgroundImage = `url(${img})`;
+  console.log("Credit for the images goes to Astronomer's Visual Pack");
+  console.log(
+    "https://forum.kerbalspaceprogram.com/topic/160878-ksp-1123-astronomers-visual-pack-beyond-v413-july-17th-2022/"
+  );
 }
 document.body.style.backgroundImage = `url(${getRandomBackgroundImage()})`;
 
 document.onload = changeBackgroundImage(getRandomBackgroundImage());
+
+console.log("Hello there! how's your day scouting your browser's console?");
