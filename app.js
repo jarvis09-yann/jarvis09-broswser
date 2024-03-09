495f20b13c85a5ca1fbb2c16846794874349cba7
jarvis09 <contact-jarvis09@laposte.net>
2024-03-09 20:52:40 +0100

    added random page anchor

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-let backGroundImages = [
+const randomSiteLink = document.getElementById("randomSiteLink");
+const backGroundImages = [
   "./images/jool3.jpg",
   "./images/eve1.jpg",
   "./images/sarnus1.jpg",
   "./images/urlum2.jpg",
 ];
 
-function getRandomBackgroundImage() {
-  const randomIndex = Math.floor(Math.random() * backGroundImages.length);
-  const randomBackgroundImage = backGroundImages[randomIndex];
-  return randomBackgroundImage;
+const randomWebPage = [
+  "https://blackmesa.jarvis09.com",
+  "https://floridaman.jarvis09.com",
+  "https://portfolio.jarvis09.com",
+];
+
+function getRandomItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomItem = array[randomIndex];
+  return randomItem;
 }
 
 function changeBackgroundImage(img) {
@@ -18,8 +25,8 @@ function changeBackgroundImage(img) {
     "https://forum.kerbalspaceprogram.com/topic/160878-ksp-1123-astronomers-visual-pack-beyond-v413-july-17th-2022/"
   );
 }
-document.body.style.backgroundImage = `url(${getRandomBackgroundImage()})`;
 
-document.onload = changeBackgroundImage(getRandomBackgroundImage());
+document.body.style.backgroundImage = `url(${getRandomItem(backGroundImages)})`;
+randomSiteLink.setAttribute("href", getRandomItem(randomWebPage));
 
 console.log("Hello there! how's your day scouting your browser's console?");
